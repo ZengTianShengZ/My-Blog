e723440d7d83e7ae54a35802aad8413e9e9ddb5d
zengtianshengz <zengtianshengz@163.com>
2018-04-01 18:59:00 +0800

    理解 HTTP 2.0

---
+++ 理解﻿ HTTP 2.0/demo/index.js
@@ -0,0 +1,48 @@
+﻿﻿/**
+ * @description: http2 node 测试
+ * @author: zengtiansheng
+ * @update: 2017/8/14
+ */
+const spdy = require('spdy');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const port = 3000;
+const app = express();
+
+app.use(express.static('src'));
+
+//  主页输出 "Hello World"
+app.get('/', function (req, res) {
+    console.log("主页 GET 请求");
+    res.send('Hello GET');
+})
+
+//  POST 请求
+app.post('/', function (req, res) {
+    console.log("主页 POST 请求");
+    res.send('Hello POST');
+})
+
+//  /del_user 页面响应
+app.get('/del_user', function (req, res) {
+    console.log("/del_user 响应 DELETE 请求");
+    res.send('删除页面');
+})
+
+const options = {
+    passphrase: '123456',
+    key: fs.readFileSync(__dirname + '/server.key'),
+    cert: fs.readFileSync(__dirname + '/server.crt')
+}
+
+spdy.createServer(options, app)
+    .listen(port, (error) => {
+        if (error) {
+            console.error(error)
+            return process.exit(1)
+        } else {
+            console.log('Listening on port: ' + port + '.')
+        }
+    })
